f53ae781691df465310efa2da18cbe7a5370bebe
olamideS12 <haddiyataffeez2@gmail.com>
2025-03-22 17:54:29 +0100

    Set and turples

---
+++ main.py
@@ -0,0 +1,9 @@
+Names=("Ally","David","Dora","Mercy","Nathan")
+
+Ally_Favcolor={"Purple","Pink","White","Red"}
+Olamide_Favcolor={"Black","White","Aqua","Purple"}
+
+print(Ally_Favcolor.union(Olamide_Favcolor))
+print(Ally_Favcolor.intersection(Olamide_Favcolor))
+print(Ally_Favcolor.difference(Olamide_Favcolor))
+print(Ally_Favcolor.symmetric_difference(Olamide_Favcolor))
